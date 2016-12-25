7f3c90b5190a3fa6175f90af3dcb5ab1edee6b0f
flashboss <luca.stancapiano@vige.it>
2016-12-25 16:32:40 +0100

    cotext spi tests

--- business-components/injection/src/main/java/it/vige/businesscomponents/injection/context/ConversationBean.java
+++ business-components/injection/src/main/java/it/vige/businesscomponents/injection/context/ConversationBean.java
@@ -2,51 +2,26 @@ package it.vige.businesscomponents.injection.context;
 
 import java.io.Serializable;
 
-import javax.annotation.PostConstruct;
 import javax.enterprise.context.Conversation;
 import javax.enterprise.context.ConversationScoped;
 import javax.inject.Inject;
 
 @ConversationScoped
-public class ConversationBean implements Serializable {
+public class ConversationBean extends Bean implements Serializable {
 
 	private static final long serialVersionUID = -5575830634866764183L;
 
 	@Inject
 	private Conversation conversation;
 
-	private int counter;
-
-	@PostConstruct
-	public void init() {
-		counter = 0;
-	}
-
 	public void initConversation() {
 		conversation.begin();
 	}
 
-	public void increment() {
-		counter++;
-	}
-
-	public String handleFirstStepSubmit() {
-		return "step2?faces-redirect=true";
-	}
-
-	public String endConversation() {
+	public void endConversation() {
 		if (!conversation.isTransient()) {
 			conversation.end();
-		}
-		return "step1?faces-redirect=true";
-	}
-
-	public int getCounter() {
-		return counter;
-	}
-
-	public void setCounter(int counter) {
-		this.counter = counter;
+		};
 	}
 
 	public Conversation getConversation() {
