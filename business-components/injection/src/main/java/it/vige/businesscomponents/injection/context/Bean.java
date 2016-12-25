d139d0499abe1dcec54f5d41bb40708fa10d6f76
flashboss <luca.stancapiano@vige.it>
2016-12-25 16:33:00 +0100

    context spi tests

---
+++ business-components/injection/src/main/java/it/vige/businesscomponents/injection/context/Bean.java
@@ -0,0 +1,15 @@
+package it.vige.businesscomponents.injection.context;
+
+public class Bean {
+
+	private int counter;
+
+	public void incrementCounter() {
+		counter++;
+	}
+
+	public int getCounter() {
+		return counter;
+	}
+
+}
