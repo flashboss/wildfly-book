21acb1c318d4673fc2f0e69240ea8e034c420679
flashboss <luca.stancapiano@vige.it>
2016-12-26 13:29:47 +0100

    event tests

--- business-components/injection/src/test/java/it/vige/businesscomponents/injection/EventTestCase.java
+++ business-components/injection/src/test/java/it/vige/businesscomponents/injection/EventTestCase.java
@@ -4,6 +4,7 @@ import static java.util.logging.Logger.getLogger;
 import static org.jboss.shrinkwrap.api.ShrinkWrap.create;
 import static org.jboss.shrinkwrap.api.asset.EmptyAsset.INSTANCE;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 import java.util.Set;
 import java.util.logging.Logger;
@@ -15,6 +16,12 @@ import javax.enterprise.inject.spi.Bean;
 import javax.enterprise.inject.spi.BeanManager;
 import javax.enterprise.inject.spi.ObserverMethod;
 import javax.inject.Inject;
+import javax.transaction.HeuristicMixedException;
+import javax.transaction.HeuristicRollbackException;
+import javax.transaction.NotSupportedException;
+import javax.transaction.RollbackException;
+import javax.transaction.SystemException;
+import javax.transaction.UserTransaction;
 
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
@@ -32,6 +39,9 @@ public class EventTestCase {
 
 	@Resource(lookup = "java:comp/BeanManager")
 	private BeanManager beanManager;
+
+	@Resource(lookup = "java:comp/UserTransaction")
+	private UserTransaction userTransaction;
 
 	@Inject
 	private Event<Bill> billEvent;
@@ -82,6 +92,28 @@ public class EventTestCase {
 		logger.info("starting size event test");
 		Set<ObserverMethod<? super Bill>> observers = beanManager.resolveObserverMethods(new Bill());
 		assertEquals(observers.size(), 10);
+	}
+
+	/**
+	 * Tests the fire of the event inside a transaction
+	 */
+	@Test
+	public void testSimpleTransaction() {
+		try {
+			userTransaction.begin();
+			Bill bill = fire();
+			assertEquals(
+					"The id generation passes through the always and it is incremented only by inprogess always observer method",
+					1, bill.getId());
+			userTransaction.commit();
+			assertEquals(
+					"The id generation passes through the always and it is incremented only by transactional observer methods",
+					4, bill.getId());
+		} catch (NotSupportedException | SystemException | SecurityException | IllegalStateException | RollbackException
+				| HeuristicMixedException | HeuristicRollbackException e) {
+			fail("no fail for the transaction");
+		}
+
 	}
 
 	private Bill fire() {
