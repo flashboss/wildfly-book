db8994351dc94c8c7f53163a675cfd9e24143e13
flashboss <luca.stancapiano@vige.it>
2017-02-04 00:32:44 +0100

    post and put

--- business-components/services/src/main/java/it/vige/businesscomponents/services/Calculator.java
+++ business-components/services/src/main/java/it/vige/businesscomponents/services/Calculator.java
@@ -4,9 +4,14 @@ import static it.vige.businesscomponents.services.Operation.DIV;
 import static it.vige.businesscomponents.services.Operation.MULTI;
 import static it.vige.businesscomponents.services.Operation.SUB;
 import static it.vige.businesscomponents.services.Operation.SUM;
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+import static javax.ws.rs.core.MediaType.TEXT_PLAIN;
 
 import javax.ejb.Stateless;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
@@ -17,31 +22,32 @@ public class Calculator {
 
 	@GET
 	@Path("/sum")
-	@Produces("text/plain")
+	@Produces(TEXT_PLAIN)
 	public double sum(@QueryParam("value") double... values) {
 		return execute(SUM, values);
 	}
 
 	@GET
 	@Path("/sub")
-	@Produces("text/plain")
+	@Produces(TEXT_PLAIN)
 	public double sub(@QueryParam("value") double... values) {
 		return execute(SUB, values);
 
 	}
 
-	@GET
+	@POST
 	@Path("/multi")
-	@Produces("text/plain")
-	public double multi(@QueryParam("value") double... values) {
+	@Consumes(APPLICATION_JSON)
+	@Produces(TEXT_PLAIN)
+	public double multi(double... values) {
 		return execute(MULTI, values);
 
 	}
 
-	@GET
-	@Path("/divide")
-	@Produces("text/plain")
-	public double divide(@QueryParam("value") double... values) {
+	@PUT
+	@Path("/div")
+	@Produces(TEXT_PLAIN)
+	public double divide(double... values) {
 		return execute(DIV, values);
 
 	}
--- business-components/services/src/test/java/it/vige/businesscomponents/services/RestTestCase.java
+++ business-components/services/src/test/java/it/vige/businesscomponents/services/RestTestCase.java
@@ -1,18 +1,26 @@
 package it.vige.businesscomponents.services;
 
+import static java.util.Arrays.asList;
 import static java.util.logging.Logger.getLogger;
 import static javax.ws.rs.client.ClientBuilder.newClient;
+import static javax.ws.rs.client.Entity.entity;
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+import static javax.ws.rs.core.MediaType.TEXT_PLAIN;
 import static org.jboss.shrinkwrap.api.ShrinkWrap.create;
 import static org.jboss.shrinkwrap.api.asset.EmptyAsset.INSTANCE;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.fail;
 
 import java.net.URL;
+import java.util.List;
 import java.util.logging.Logger;
 
 import javax.net.ssl.HostnameVerifier;
+import javax.ws.rs.ProcessingException;
 import javax.ws.rs.client.Client;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.Response;
 
@@ -48,8 +56,41 @@ public class RestTestCase {
 		WebTarget target = client.target(url + "services/calculator/sum?value=4&value=6");
 		Response response = target.request().get();
 		double value = response.readEntity(Double.class);
-		response.close(); // You should close connections!
+		response.close();
 		assertEquals("sum implemented: ", 10.0, value, 0.0);
+	}
+
+	@Test
+	public void testJaxRSPost() throws Exception {
+		logger.info("start JaxRS Post test");
+		Client client = newClient();
+		WebTarget target = client.target(url + "services/calculator/multi");
+		double value = 0.0;
+		Response response = null;
+		try {
+			Entity<double[]> valuesAsArray = entity(new double[] { 4.5, 6.7 }, TEXT_PLAIN);
+			target.request().post(valuesAsArray);
+			fail();
+		} catch (ProcessingException ex) {
+			assertEquals("Arrays not supported", "RESTEASY004655: Unable to invoke request", ex.getMessage());
+		}
+		try {
+			Entity<List<Double>> valuesAsList = entity(asList(new Double[] { 4.5, 6.7 }), TEXT_PLAIN);
+			response = target.request().post(valuesAsList);
+			value = response.readEntity(Double.class);
+		} catch (ProcessingException ex) {
+			assertEquals("TEXT_PLAIN not supported",
+					"RESTEASY003145: Unable to find a MessageBodyReader of content-type */* and type class java.lang.Double",
+					ex.getMessage());
+		} finally {
+			response.close();
+		}
+
+		Entity<List<Double>> valuesAsList = entity(asList(new Double[] { 4.5, 6.7 }), APPLICATION_JSON);
+		response = target.request().post(valuesAsList);
+		value = response.readEntity(Double.class);
+		response.close();
+		assertEquals("sum implemented: ", 30.150000000000002, value, 0.0);
 	}
 
 	@Test
@@ -59,8 +100,20 @@ public class RestTestCase {
 		WebTarget target = client.target(url + "services/calculator/sub?value=40&value=6");
 		Response response = target.request().get();
 		double value = response.readEntity(Double.class);
-		response.close(); // You should close connections!
+		response.close();
 		assertEquals("subtract implemented: ", 34.0, value, 0.0);
+	}
+
+	@Test
+	public void testJaxRSPut() throws Exception {
+		logger.info("start JaxRS Put test");
+		Client client = newClient();
+		WebTarget target = client.target(url + "services/calculator/div");
+		Entity<List<Double>> valuesAsList = entity(asList(new Double[] { 4.5, 6.7 }), APPLICATION_JSON);
+		Response response = target.request().put(valuesAsList);
+		double value = response.readEntity(Double.class);
+		response.close();
+		assertEquals("sum implemented: ", 0.6716417910447761, value, 0.0);
 	}
 
 	@Test
