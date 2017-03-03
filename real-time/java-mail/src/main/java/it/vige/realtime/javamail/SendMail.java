c1e50f1b166f53068b24318c311c1bd1f65e33f3
flashboss <luca.stancapiano@vige.it>
2017-03-03 14:23:04 +0100

    client mail

--- real-time/java-mail/src/main/java/it/vige/realtime/javamail/SendMail.java
+++ real-time/java-mail/src/main/java/it/vige/realtime/javamail/SendMail.java
@@ -24,33 +24,33 @@ public class SendMail {
 	private Session getMailSession;
 	private MimeMessage generateMailMessage;
 
-	public void completeClientSend() throws AddressException, MessagingException {
+	public void completeClientSend(String... credentials) throws AddressException, MessagingException {
 		// Step1
-		logger.info("\n 1st ===> setup Mail Server Properties..");
-		mailServerProperties = getProperties();
-		mailServerProperties.put("mail.smtp.port", "587");
-		mailServerProperties.put("mail.smtp.auth", "true");
-		mailServerProperties.put("mail.smtp.starttls.enable", "true");
-		logger.info("Mail Server Properties have been setup successfully..");
-		// Step2
-		logger.info("\n\n 2nd ===> get Mail .");
-		getMailSession = getDefaultInstance(mailServerProperties, null);
-		generateMailMessage = new MimeMessage(getMailSession);
-		generateMailMessage.addRecipient(TO, new InternetAddress("test1@vige.it"));
-		generateMailMessage.addRecipient(CC, new InternetAddress("test2@vige.it"));
-		generateMailMessage.setSubject("Greetings from Vige..");
-		String emailBody = "Test email by Vige.it JavaMail API example. "
-				+ "<br><br> Regards, <br>Vige Admin";
-		generateMailMessage.setContent(emailBody, "text/html");
-		logger.info("Mail Session has been created successfully..");
-		// Step3
-		logger.info("\n\n 3rd ===> Get Session and Send mail");
-		Transport transport = getMailSession.getTransport("smtp");
-		// Enter your correct gmail UserID and Password
-		// if you have 2FA enabled then provide App Specific Password
-		transport.connect("smtp.gmail.com", "<----- Your GMAIL ID ----->", "<----- Your GMAIL PASSWORD ----->");
-		transport.sendMessage(generateMailMessage, generateMailMessage.getAllRecipients());
-		transport.close();
-
+		if (credentials != null) {
+			logger.info("\n 1st ===> setup Mail Server Properties..");
+			mailServerProperties = getProperties();
+			mailServerProperties.put("mail.smtp.port", "587");
+			mailServerProperties.put("mail.smtp.auth", "true");
+			mailServerProperties.put("mail.smtp.starttls.enable", "true");
+			logger.info("Mail Server Properties have been setup successfully..");
+			// Step2
+			logger.info("\n\n 2nd ===> get Mail .");
+			getMailSession = getDefaultInstance(mailServerProperties, null);
+			generateMailMessage = new MimeMessage(getMailSession);
+			generateMailMessage.addRecipient(TO, new InternetAddress("test1@vige.it"));
+			generateMailMessage.addRecipient(CC, new InternetAddress("test2@vige.it"));
+			generateMailMessage.setSubject("Greetings from Vige..");
+			String emailBody = "Test email by Vige.it JavaMail API example. " + "<br><br> Regards, <br>Vige Admin";
+			generateMailMessage.setContent(emailBody, "text/html");
+			logger.info("Mail Session has been created successfully..");
+			// Step3
+			logger.info("\n\n 3rd ===> Get Session and Send mail");
+			Transport transport = getMailSession.getTransport("smtp");
+			// Enter your correct gmail UserID and Password
+			// if you have 2FA enabled then provide App Specific Password
+			transport.connect("smtp.gmail.com", credentials[0], credentials[1]);
+			transport.sendMessage(generateMailMessage, generateMailMessage.getAllRecipients());
+			transport.close();
+		}
 	}
 }
